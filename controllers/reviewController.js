89fdb6690f1335bfee79189953285109f69bce1d
srinivaskalagotla123 <srineehu@gmail.com>
2024-01-28 22:36:21 +0530

    initial commit

---
+++ controllers/reviewController.js
@@ -0,0 +1,17 @@
+const Review = require('./../models/reviewModel');
+//const catchAsync = require('./../utils/catchAsync');
+const factory = require('./handlerFactory');
+// eslint-disable-next-line node/no-unsupported-features/es-syntax
+// eslint-disable-next-line node/no-unsupported-features/es-syntax
+exports.setTourUserIds = (req, res, next) => {
+  // Allow nested routes
+  if (!req.body.tour) req.body.tour = req.params.tourId;
+  if (!req.body.user) req.body.user = req.user.id;
+  next();
+};
+exports.getAllReviews = factory.getAll(Review);
+
+exports.createReview = factory.createOne(Review);
+exports.getReview = factory.getOne(Review);
+exports.deleteReview = factory.deleteOne(Review);
+exports.updateReview = factory.updateOne(Review);
